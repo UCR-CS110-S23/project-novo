d887e4fa98eb23c7d6fa5ecbd91c917b8fa13e62
Robert Lerias <72523111+bobbyyy57@users.noreply.github.com>
2023-05-16 02:15:36 -0700

    added novo purple

--- components/UserCard.jsx
+++ components/UserCard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Viviane from "../public/viviane.png";
+import { GrLocation } from "react-icons/gr";
 
 const UserCard = () => {
 	return (
@@ -12,8 +13,22 @@ const UserCard = () => {
 					layout='responsive'
 					className='rounded-t-3xl'
 				/>
-				<div className='border border-gray-300'>
-					<div className='text-5xl'>Viviane, 22</div>
+				<div className='border border-gray-300 p-5'>
+					<div className='flex justify-between items-center'>
+						<div className='text-4xl font-medium'>Viviane, 22</div>
+						<div className='flex items-center text-md bg-gray-200 py-2 px-5 rounded-l-full pr-10'>
+							<span className='pr-1.5'>
+								<GrLocation />
+							</span>
+							Riverside, CA
+						</div>
+					</div>
+					<div>tags tags tags tags tags</div>
+					<div className='font-light'>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+						sed do eiusmod tempor incididunt ut labore et dolore
+						magna aliqua. Ut enim ad minim veniam,.
+					</div>
 				</div>
 			</div>
 		</>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
 				"gradient-conic":
 					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
 			},
+			colors: {
+				novo: {
+					purple: "#7231F3",
+				},
+			},
 		},
 	},
 	plugins: [],
