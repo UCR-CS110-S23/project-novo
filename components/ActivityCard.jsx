2c08ff57760ae57347e8a170da98e1aa5dc25872
Robert Lerias <72523111+bobbyyy57@users.noreply.github.com>
2023-05-16 11:40:22 -0700

    activity card comp done

--- components/ActivityCard.jsx
+++ components/ActivityCard.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Image from "next/image";
+import Disney from "../public/disneyland.png";
 
 const ActivityCard = () => {
-	return <div>ActivityCard</div>;
+	return (
+		<>
+			<div className='grid rows-2 w-1/5 h-fit'>
+				<Image
+					src={Disney}
+					alt='Landing'
+					layout='responsive'
+					className='rounded-t-3xl bg-red-400'
+				/>
+				<div className='border border-novo-lightgray rounded-b-3xl px-3.5 py-1'>
+					So cute. So nostalgic. Take me.
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default ActivityCard;
--- pages/feed.js
+++ pages/feed.js
@@ -1,9 +1,11 @@
+import ActivityCard from "../components/ActivityCard.jsx";
 import UserCard from "../components/UserCard.jsx";
 
 export default function Feed() {
 	return (
 		<div className='flex justify-center'>
 			<UserCard />
+			<ActivityCard />
 		</div>
 	);
 }
