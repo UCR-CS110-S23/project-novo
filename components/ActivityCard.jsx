ac09ed98292c709c3674271f85410919f1fc1374
Robert Lerias <72523111+bobbyyy57@users.noreply.github.com>
2023-05-16 12:24:43 -0700

    finished styling activity card

--- components/ActivityCard.jsx
+++ components/ActivityCard.jsx
@@ -5,14 +5,14 @@ import Disney from "../public/disneyland.png";
 const ActivityCard = () => {
 	return (
 		<>
-			<div className='grid rows-2 w-1/5 h-fit'>
+			<div className='grid rows-2 h-fit'>
 				<Image
 					src={Disney}
 					alt='Landing'
 					layout='responsive'
-					className='rounded-t-3xl bg-red-400'
+					className='rounded-t-3xl bg-orange-500'
 				/>
-				<div className='border border-novo-lightgray rounded-b-3xl px-3.5 py-1'>
+				<div className='border border-novo-lightgray bg-white text-sm text-center rounded-b-3xl px-3.5 py-1'>
 					So cute. So nostalgic. Take me.
 				</div>
 			</div>
--- pages/feed.js
+++ pages/feed.js
@@ -4,8 +4,18 @@ import UserCard from "../components/UserCard.jsx";
 export default function Feed() {
 	return (
 		<div className='flex justify-center'>
-			<UserCard />
-			<ActivityCard />
+			<div className='w-1/3'>
+				<UserCard />
+			</div>
+			<div className='w-[28vw] ml-5 flex flex-col space-y-3'>
+				<ActivityCard />
+				<ActivityCard />
+				<ActivityCard />
+			</div>
+
+			{/* <div className='bg-red-500 h-fit w-fit flex flex-col'> */}
+
+			{/* </div> */}
 		</div>
 	);
 }
