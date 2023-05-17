c6fb38d876f796b14f187b14d5522f1c43d51f29
Robert Lerias <72523111+bobbyyy57@users.noreply.github.com>
2023-05-17 15:12:48 -0700

    removed selection visual

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import { React } from "react";
 import Image from "next/image";
 import NOVO from "../public/NOVO.png";
 import { RiCompassDiscoverLine } from "react-icons/ri";
@@ -9,7 +9,6 @@ import { RxExit } from "react-icons/rx";
 import Link from "next/link";
 
 function NavBar() {
-	const [toggle, setToggle] = useState(0);
 	return (
 		<>
 			<div className='flex flex-col justify-between h-screen static bg-novo-purple rounded-r-xl'>
@@ -25,35 +24,18 @@ function NavBar() {
 					<div className='flex flex-col justify-start w-full space-y-2 '>
 						<Link
 							href='/feed'
-							onClick={() => setToggle(1)}
-							className={`text-white flex w-full no-underline items-center py-2 px-4  space-x-2 font-light ${
-								toggle === 1
-									? "bg-novo-selectPurple text-white rounded-xl"
-									: "bg-transparent"
-							}`}
+							className='text-white flex w-full no-underline items-center py-2 px-4   space-x-2 font-light'
 						>
 							<RiCompassDiscoverLine />
 							<div>Feed</div>
 						</Link>
-						<button
-							onClick={() => setToggle(2)}
-							className={`text-white flex w-full items-center no-underline py-2 px-4  space-x-2 font-light ${
-								toggle === 2
-									? "bg-novo-selectPurple text-white rounded-xl"
-									: "bg-transparent"
-							}`}
-						>
+						<button className='text-white flex w-full items-center no-underline py-2 px-4    space-x-2 font-light'>
 							<FiMessageSquare />
 							<div>Messages</div>
 						</button>
 						<Link
 							href='/activityProfile'
-							onClick={() => setToggle(3)}
-							className={`text-white flex w-full items-center  no-underline py-2 px-4  space-x-2 font-light ${
-								toggle === 3
-									? "bg-novo-selectPurple text-white rounded-xl"
-									: "bg-transparent"
-							}`}
+							className='text-white flex w-full items-center  no-underline py-2 px-4    space-x-2 font-light'
 						>
 							<TbCircles />
 							<div>Activities</div>
