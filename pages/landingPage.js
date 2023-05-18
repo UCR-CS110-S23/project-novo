ed17a5a2e59edac5a48c36794d5f19d323b8aed0
Paulian Le <paulianle7@gmail.com>
2023-05-18 01:34:47 -0700

    Basic text and button for landing page added

---
+++ pages/landingPage.js
@@ -0,0 +1,11 @@
+export default function LandingPage() {
+	return (
+		<div className='flex flex-col items-center justify-center h-screen'>
+			<h1 className='text-4xl font-bold'>NOVO</h1>
+			<h2 className='text-lg'>Meet your new adventure</h2>
+			<button className='mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg'>
+				Sign Up
+			</button>
+		</div>
+	);
+}
