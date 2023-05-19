f21b91f2a97f6b423b230713d0afbc3009e465df
Paulian Le <paulianle7@gmail.com>
2023-05-18 23:02:35 -0700

    Added nav bar

---
+++ pages/editProfile.js
@@ -0,0 +1,15 @@
+import NavBar from "../components/NavBar";
+
+export default function EditProfile() {
+	return (
+		<>
+			<div>
+				<div className='grid grid-cols-6'>
+					<div className='col-span-1'>
+						<NavBar />
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
