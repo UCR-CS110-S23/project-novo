8a213d4ad422d0500edf938b1796d766687cd5b7
Paulian Le <paulianle7@gmail.com>
2023-05-18 22:14:40 -0700

    Added comp to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
-// import Profile from "../components/Profile.jsx";
 import WelcomeText from "../components/WelcomeText.jsx";
 
 export default function Home() {
 	return (
-		<div class="w-full h-screen bg-no-repeat bg-cover bg-[url('../public/landing-back.png')]">
+		<div className="w-full h-screen bg-no-repeat bg-cover bg-[url('../public/landing-back.png')]">
 			<div>
-				{/* Hello! */}
-				{/* <Profile /> */}
 				<WelcomeText />
 			</div>
 		</div>
