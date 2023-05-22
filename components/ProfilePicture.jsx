7056ac2d31e30233a16a7edda39c325e4b0717cd
Paulian Le <paulianle7@gmail.com>
2023-05-22 14:27:11 -0700

    Rough profpic comp added

--- components/ProfilePicture.jsx
+++ components/ProfilePicture.jsx
@@ -1,11 +1,21 @@
-import React from "react";
-// import UploadPhoto from "../public/uploadPhoto.png";
-// import Image from "next/image";
+import Image from "next/image";
+import UploadPhoto from "../public/uploadPhoto.png";
 
 const ProfilePicture = () => {
 	return (
 		<>
-			<div></div>
+			<div className='bg-[#F0F0F0] rounded-3xl aspect-square flex justify-center items-center flex-col w-3/12'>
+				<div className='w-5/12'>
+					<Image
+						src={UploadPhoto}
+						alt='ProfileUploadPicture'
+						layout='responsive'
+					/>
+				</div>
+				<button className='border border-[#858585] py-2 px-3 mt-4 text-[#858585] rounded-3xl text-xs'>
+					UPLOAD FROM COMPUTER
+				</button>
+			</div>
 		</>
 	);
 };
--- pages/editProfile.js
+++ pages/editProfile.js
@@ -1,12 +1,27 @@
 import NavBar from "../components/NavBar";
+import ProfilePicture from "../components/ProfilePicture.jsx";
+// import AddPicture from "../components/AddPicture.jsx";
 
 export default function EditProfile() {
 	return (
 		<>
-			<div>
-				<div className='grid grid-cols-6'>
-					<div className='col-span-1'>
+			<div className='grid grid-cols-6'>
+				{/* Nav Bar */}
+				<div className='col-span-1 w-1/6 fixed'>
+					<div className='absolute '>
 						<NavBar />
+					</div>
+				</div>
+
+				{/* Page Title */}
+				<div className='col-start-2 col-end-6'>
+					<div className='mt-8 text-left'>
+						<div className='text-2xl ml-40 mt-14'>Edit Profile</div>
+					</div>
+
+					{/* Adding Photos Part */}
+					<div className='ml-40 mt-14'>
+						<ProfilePicture />
 					</div>
 				</div>
 			</div>
