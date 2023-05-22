114f69afb5660033f0658ae13530740a71efdeb4
Paulian Le <paulianle7@gmail.com>
2023-05-22 14:14:41 -0700

    Added temp comp code for first half

---
+++ components/AddPicture.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AddSymbol from "../public/addSymbol.png";
+import Image from "next/image";
+
+const AddPicture = () => {
+	return (
+		<>
+			<div></div>
+		</>
+	);
+};
+
+export default AddPicture;
---
+++ components/ProfilePicture.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import UploadPhoto from "../public/uploadPhoto.png";
+import Image from "next/image";
+
+const ProfilePicture = () => {
+	return (
+		<>
+			<div></div>
+		</>
+	);
+};
+
+export default ProfilePicture;
