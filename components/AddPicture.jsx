81973290ea3cf65a3a1293b9d573aa621ae3968f
Paulian Le <paulianle7@gmail.com>
2023-05-23 15:53:57 -0700

    Add pic component done

--- components/AddPicture.jsx
+++ components/AddPicture.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-// import AddSymbol from "../public/addSymbol.png";
-// import Image from "next/image";
+import Image from "next/image";
+import AddSymbol from "../public/addSymbol.png";
 
 const AddPicture = () => {
 	return (
 		<>
-			<div></div>
+			<button className='border border-[#858585] rounded-3xl aspect-square flex justify-center items-center flex-col w-3/12'>
+				<div className='w-2/12'>
+					<Image src={AddSymbol} alt='Landing' layout='responsive' />
+				</div>
+			</button>
 		</>
 	);
 };
