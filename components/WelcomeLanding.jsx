e2bd604bcc00cbbd268a6b22e9a47a9802a12db1
Alamode89 <andreamante89@gmail.com>
2023-05-26 03:58:00 -0700

    Added input fields

--- components/WelcomeLanding.jsx
+++ components/WelcomeLanding.jsx
@@ -2,33 +2,52 @@ import React from "react";
 import NOVO from "../public/NOVO.png";
 import Image from "next/image";
 import { signIn } from "next-auth/react";
-const WelcomeText = () => {
-	return (
-		<>
-			<div className='flex flex-col items-center justify-center h-screen'>
-				<div className='flex justify-center items-center'>
-					<Image
-						src={NOVO}
-						alt='site-logo'
-						className='max-w-md pb-4'
-					/>
-				</div>
-				<h2 className='text-2xl text-white font-light'>
-					meet your <b>new</b> adventure
-				</h2>
+import { useState } from "react";
 
-				<div className='group'>
-					<button
-						className='text-white mt-5 px-4 py-2 w-60 rounded-full border border-white transition-colors duration-300 group-hover:bg-white'
-						onClick={() => signIn("google")}
-					>
-						<span className='group-hover:text-purple-500'>
-							SIGN UP
-						</span>
-					</button>
-				</div>
+const WelcomeText = () => {
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	return (
+		<div className='flex flex-row items-center justify-center h-screen'>
+			<div className='flex flex-col justify-center items-center'>
+				<Image src={NOVO} alt='site-logo' className='max-w-md pb-4' />
+				<p className='text-2xl text-white font-light'>
+					meet your <b>new</b> adventure
+				</p>
 			</div>
-		</>
+
+			<div className='flex flex-col'>
+				<input
+					className='rounded-full opacity-10 text-white placeholder-black'
+					type='text'
+					placeholder='NAME'
+					value={name}
+					onChange={e => setName(e.target.value)}
+				/>
+				<input
+					className=''
+					type='text'
+					placeholder='EMAIL'
+					value={email}
+					onChange={e => setEmail(e.target.value)}
+				/>
+				<input
+					className=''
+					type='text'
+					placeholder='PASSWORD'
+					value={password}
+					onChange={e => setPassword(e.target.value)}
+				/>
+				<button
+					className='text-white mt-5 px-4 py-2 w-60 rounded-full border border-white transition-colors duration-300 hover:bg-white hover:text-black'
+					onClick={() => signIn("google")}
+				>
+					SIGN UP
+				</button>
+			</div>
+		</div>
 	);
 };
 
