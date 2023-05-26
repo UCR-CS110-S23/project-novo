dbeed31fc610f16aa5659d1d8064689ca5c495ea
Alamode89 <andreamante89@gmail.com>
2023-05-26 05:03:06 -0700

    Or split

--- components/WelcomeLanding.jsx
+++ components/WelcomeLanding.jsx
@@ -10,8 +10,8 @@ const WelcomeText = () => {
 	const [password, setPassword] = useState("");
 
 	return (
-		<div className='flex flex-row items-center justify-center h-screen'>
-			<div className='flex flex-col justify-center items-center'>
+		<div className='flex flex-row items-center justify-between mx-80 h-screen'>
+			<div className='flex flex-col'>
 				<Image src={NOVO} alt='site-logo' className='max-w-md pb-4' />
 				<p className='text-2xl text-white font-light'>
 					meet your <b>new</b> adventure
@@ -20,32 +20,37 @@ const WelcomeText = () => {
 
 			<div className='flex flex-col'>
 				<input
-					className='rounded-full opacity-10 text-white placeholder-black'
+					className='rounded-full bg-white bg-opacity-10 text-white placeholder-white py-3 pl-4 pr-10 w-80 focus:outline-none my-1'
 					type='text'
 					placeholder='NAME'
 					value={name}
 					onChange={e => setName(e.target.value)}
 				/>
 				<input
-					className=''
+					className='rounded-full bg-white bg-opacity-10 text-white placeholder-white py-3 pl-4 pr-10 w-80 focus:outline-none my-1'
 					type='text'
 					placeholder='EMAIL'
 					value={email}
 					onChange={e => setEmail(e.target.value)}
 				/>
 				<input
-					className=''
+					className='rounded-full bg-white bg-opacity-10 text-white placeholder-white py-3 pl-4 pr-10 w-80 focus:outline-none my-1'
 					type='text'
 					placeholder='PASSWORD'
 					value={password}
 					onChange={e => setPassword(e.target.value)}
 				/>
 				<button
-					className='text-white mt-5 px-4 py-2 w-60 rounded-full border border-white transition-colors duration-300 hover:bg-white hover:text-black'
+					className='mt-3 text-white px-4 py-2 w-60 rounded-full border border-white transition-colors duration-300 hover:bg-white hover:text-black'
 					onClick={() => signIn("google")}
 				>
 					SIGN UP
 				</button>
+				<div className='flex items-center my-4'>
+					<hr className='flex-grow border-gray-300 border-t-2' />
+					<div className='mx-4 text-gray-500'>or</div>
+					<hr className='flex-grow border-gray-300 border-t-2' />
+				</div>
 			</div>
 		</div>
 	);
