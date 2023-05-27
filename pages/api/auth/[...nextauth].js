87dcd0194ba56522901dcd7a1bc9e9ae8c1802ed
Alamode89 <andreamante89@gmail.com>
2023-05-26 20:34:57 -0700

    bless up

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,9 +4,6 @@ import clientPromise from "/lib/mongodb.js";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 
 export default async function auth(req, res) {
-	const adapter = await MongoDBAdapter({
-		db: await clientPromise,
-	});
 	return await NextAuth(req, res, {
 		providers: [
 			GoogleProvider({
@@ -14,34 +11,34 @@ export default async function auth(req, res) {
 				clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 			}),
 		],
-		adapter: adapter,
-		callbacks: {
-			async signIn({ user, account, profile }) {
-				if (account.provider === "google") {
-					user.name = {
-						first: String(profile.name.split(" ")[0]),
-						last: String(profile.name.split(" ")[1]),
-					};
-				}
-				return true;
-			},
-			async session({ session, user }) {
-				return {
-					...session,
-					user: {
-						...session.user,
-						uid: user.uid,
-						name: user.name,
-						photoURL: user.photoURL,
-						bio: user.bio,
-						age: user.age,
-					},
-				};
-			},
-		},
-		pages: {
-			signIn: "/feed",
-		},
+		adapter: MongoDBAdapter(clientPromise),
+		// callbacks: {
+		// 	async signIn({ user, account, profile }) {
+		// 		if (account.provider === "google") {
+		// 			user.name = {
+		// 				first: String(profile.name.split(" ")[0]),
+		// 				last: String(profile.name.split(" ")[1]),
+		// 			};
+		// 		}
+		// 		return true;
+		// 	},
+		// 	async session({ session, user }) {
+		// 		return {
+		// 			...session,
+		// 			user: {
+		// 				...session.user,
+		// 				uid: user.uid,
+		// 				name: user.name,
+		// 				photoURL: user.photoURL,
+		// 				bio: user.bio,
+		// 				age: user.age,
+		// 			},
+		// 		};
+		// 	},
+		// },
+		// pages: {
+		// 	signIn: "/feed",
+		// },
 		secret: process.env.JWT_SECRET,
 	});
 }
