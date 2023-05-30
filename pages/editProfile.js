b63c717999d986c83ffcb58da533b177816fb640
Paulian Le <paulianle7@gmail.com>
2023-05-29 18:07:42 -0700

    Styling for interests and interests tag done

--- pages/editProfile.js
+++ pages/editProfile.js
@@ -236,15 +236,15 @@ export default function EditProfile() {
 					</div>
 
 					{/* Interests --> user types in any interest --> populates in this box */}
-					<div className='ml-40 w-full'>
-						<Row className='mt-4 ml-40 focus:outline-none w-full h-[13vh] border rounded-2xl px-4 py-3 resize-none placeholder:font-light placeholder-[#858585] placeholder:font-regular'>
+					<div className='ml-40 w-full mt-4 focus:outline-none h-[15vh] border rounded-2xl px-4 py-3 resize-none placeholder:font-light placeholder-[#858585] placeholder:font-regular'>
+						<Row className=''>
 							{[...data.interests].map((interest, index) => (
 								<Col key={index} className='!max-w-fit p-1'>
 									<div className=''>
 										<button className='text-white border-[#7231F3] px-3 bg-[#7231F3] py-1 rounded-full flex justify-center items-center'>
 											{interest}
 											<FaTimes
-												className='hover:text-red-500 ml-2'
+												className='hover:text-red-500 ml-4'
 												onClick={() =>
 													handleTagRemove(interest)
 												}
