2219a4b4b203ec0afea3830cbecff3c020f8073c
Robert Lerias <72523111+bobbyyy57@users.noreply.github.com>
2023-06-06 23:25:28 -0700

    modal pops up now

--- components/MatchModal.jsx
+++ components/MatchModal.jsx
@@ -1,56 +1,58 @@
-import React from "react";
-import Link from "next/link";
-import { GrClose } from "react-icons/gr";
-import Image from "next/image.js";
+// import React from "react";
+// import Link from "next/link";
+// import { GrClose } from "react-icons/gr";
+// import Image from "next/image.js";
+// import ActivitySelected from "../public/ActivitySelected.png";
 
-const MatchModal = () => {
-	return (
-		<>
-			<div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50'>
-				<div className=' w-auto my-6  mx-auto max-w-3xl'>
-					<div className='border-0  rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
-						<div className='flex flex-col place-items-center p-6'>
-							<div className='text-2xl w-full flex justify-end'>
-								<button onClick={() => setShowModal(false)}>
-									<GrClose />
-								</button>
-							</div>
-							<div className='flex flex-row'>
-								<div className='text-4xl font-medium'>
-									Activity Selected
-								</div>
-							</div>
-							<div className='text-sm text-novo-messagegray mb-4 mt-1 font-light'>
-								GO TO MESSAGES TO START CONVERSATION WITH
-								VIVIANE
-							</div>
-							<div className='mx-24'>
-								<div className='relative w-96 flex justify-center items-center'>
-									<Image
-										src={ActivitySelected}
-										alt='Landing'
-										layout='responsive'
-										className='rounded-xl'
-									/>
-									<div className='absolute bg-white rounded-full text-black text-md px-4 py-2'>
-										DISNEYLAND
-									</div>
-								</div>
-							</div>
-							<div>
-								<Link href='/messaging'>
-									<button className='bg-novo-purple hover:bg-novo-darkpurple w-48 rounded-full text-white mt-8 px-3 py-0.5 mb-4 font-light text-l'>
-										Message
-									</button>
-								</Link>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div className='fixed bg-gray-600 bg-opacity-50 inset-0'></div>
-		</>
-	);
-};
+// const MatchModal = () => {
+//     const [showModal, setShowModal] = useState(false);
+// 	return (
+// 		<>
+// 			<div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50'>
+// 				<div className=' w-auto my-6  mx-auto max-w-3xl'>
+// 					<div className='border-0  rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
+// 						<div className='flex flex-col place-items-center p-6'>
+// 							<div className='text-2xl w-full flex justify-end'>
+// 								<button onClick={() => setShowModal(false)}>
+// 									<GrClose />
+// 								</button>
+// 							</div>
+// 							<div className='flex flex-row'>
+// 								<div className='text-4xl font-medium'>
+// 									Activity Selected
+// 								</div>
+// 							</div>
+// 							<div className='text-sm text-novo-messagegray mb-4 mt-1 font-light'>
+// 								GO TO MESSAGES TO START CONVERSATION WITH
+// 								VIVIANE
+// 							</div>
+// 							<div className='mx-24'>
+// 								<div className='relative w-96 flex justify-center items-center'>
+// 									<Image
+// 										src={ActivitySelected}
+// 										alt='Landing'
+// 										layout='responsive'
+// 										className='rounded-xl'
+// 									/>
+// 									<div className='absolute bg-white rounded-full text-black text-md px-4 py-2'>
+// 										DISNEYLAND
+// 									</div>
+// 								</div>
+// 							</div>
+// 							<div>
+// 								<Link href='/messaging'>
+// 									<button className='bg-novo-purple hover:bg-novo-darkpurple w-48 rounded-full text-white mt-8 px-3 py-0.5 mb-4 font-light text-l'>
+// 										Message
+// 									</button>
+// 								</Link>
+// 							</div>
+// 						</div>
+// 					</div>
+// 				</div>
+// 			</div>
+// 			<div className='fixed bg-gray-600 bg-opacity-50 inset-0'></div>
+// 		</>
+// 	);
+// };
 
-export default MatchModal;
+// export default MatchModal;
--- pages/feed.js
+++ pages/feed.js
@@ -2,18 +2,22 @@
 import ActivityCard from "../components/ActivityCards.jsx";
 import UserCard from "../components/UserCard.jsx";
 import NavBar from "../components/NavBar";
+import { GrClose } from "react-icons/gr";
 import { getAllPostData } from "@/lib/getFeed";
 import { useState } from "react";
+import ActivitySelected from "../public/ActivitySelected.png";
 import { RxCross2 } from "react-icons/rx";
 import { BsCheckLg } from "react-icons/bs";
-import MatchModal from "@/components/MatchModal.jsx";
+// import MatchModal from "@/components/MatchModal.jsx";
+import Link from "next/link.js";
+import Image from "next/image.js";
 
 export default function Feed({ data }) {
 	const temp = JSON.parse(data);
 	console.log("DATA", temp);
 
 	const [counter, setCounter] = useState(0);
-	// const [showModal, setShowModal] = useState(false);
+	const [showModal, setShowModal] = useState(false);
 
 	const increase = () => {
 		if (counter < temp.length) setCounter(counter => counter + 1);
@@ -23,7 +27,7 @@ export default function Feed({ data }) {
 
 	const handleCheck = () => {
 		increase();
-		<MatchModal />;
+		setShowModal(true);
 	};
 
 	return (
@@ -62,6 +66,55 @@ export default function Feed({ data }) {
 					</div>
 				</div>
 			</div>
+			{showModal ? (
+				<>
+					<div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50'>
+						<div className=' w-auto my-6  mx-auto max-w-3xl'>
+							<div className='border-0  rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
+								<div className='flex flex-col place-items-center p-6'>
+									<div className='text-2xl w-full flex justify-end'>
+										<button
+											onClick={() => setShowModal(false)}
+										>
+											<GrClose />
+										</button>
+									</div>
+									<div className='flex flex-row'>
+										<div className='text-4xl font-medium'>
+											Activity Selected
+										</div>
+									</div>
+									<div className='text-sm text-novo-messagegray mb-4 mt-1 font-light'>
+										GO TO MESSAGES TO START CONVERSATION
+										WITH VIVIANE
+									</div>
+									<div className='mx-24'>
+										<div className='relative w-96 flex justify-center items-center'>
+											<Image
+												src={ActivitySelected}
+												alt='Landing'
+												layout='responsive'
+												className='rounded-xl'
+											/>
+											<div className='absolute bg-white rounded-full text-black text-md px-4 py-2'>
+												DISNEYLAND
+											</div>
+										</div>
+									</div>
+									<div>
+										<Link href='/messaging'>
+											<button className='bg-novo-purple hover:bg-novo-darkpurple w-48 rounded-full text-white mt-8 px-3 py-0.5 mb-4 font-light text-l'>
+												Message
+											</button>
+										</Link>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className='fixed bg-gray-600 bg-opacity-50 inset-0'></div>
+				</>
+			) : null}
 		</div>
 	);
 }
