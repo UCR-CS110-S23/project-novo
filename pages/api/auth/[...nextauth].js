bb093efd7f7ee3b926f845067ad740afb405bb47
Alamode89 <andreamante89@gmail.com>
2023-06-06 20:54:20 -0700

    Credentials works but google decided to break lol

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -21,19 +21,19 @@ export const authOptions = {
 			name: "Credentials",
 			async authorize(credentials) {
 				const db = (await clientPromise).db(process.env.MONGODB_DB);
-				const existingUser = await db
+				const user = await db
 					.collection("users")
 					.findOne({ email: credentials.email });
 
 				// Checks if email exists
-				if (!existingUser) {
+				if (!user) {
 					console.log("Authentication: Email not found");
 					return null;
 				}
 
 				const validPassword = await isPasswordValid(
 					credentials.password,
-					existingUser.password
+					user.password
 				);
 
 				if (!validPassword) {
@@ -42,60 +42,54 @@ export const authOptions = {
 				}
 
 				return {
-					name: existingUser.name,
-					email: existingUser.email,
+					uid: user._id,
+					name: user.name,
+					email: user.email,
+					image: user.image,
+					age: user.age,
+					pronoun: user.pronoun,
+					gender: user.gender,
+					bio: user.bio,
+					interests: user.interests,
+					location: user.location,
+					preference: user.preference,
 				};
 			},
 		}),
 	],
-	// callbacks: {
-	// 	async signIn({ user, account, profile }) {
-	// 		if (account.provider === "google") {
-	// 			user.name = {
-	// 				first: String(profile.name.split(" ")[0]),
-	// 				last: String(profile.name.split(" ")[1]),
-	// 			};
-	// 		}
-	// 		return true;
-	// 	},
-	// 	async session({ session, user }) {
-	// 		return {
-	// 			...session,
-	// 			user: {
-	// 				...session.user,
-	// 				uid: user.uid,
-	// 				name: user.name,
-	// 				photoURL: user.photoURL,
-	// 				bio: user.bio,
-	// 				age: user.age,
-	// 			},
-	// 		};
-	// 	},
-	// },
 	secret: process.env.JWT_SECRET,
+	session: {
+		strategy: "jwt",
+	},
+	callbacks: {
+		async signIn({ user, account }) {
+			if (account.provider === "google") {
+				user._id = account.providerAccountId;
+				user.name = {
+					first: String(profile.name.split(" ")[0]),
+					last: String(profile.name.split(" ")[1]),
+				};
+				user.provider = account.provider;
+			}
+
+			return true;
+		},
+		async jwt({ token, user, session, trigger }) {
+			if (user) {
+				token.user = user;
+			}
+
+			if (trigger === "update" && session?.name) {
+				token.user.name = session.name;
+			}
+
+			return token;
+		},
+		async session({ session, token }) {
+			session.user = token.user;
+			return session;
+		},
+	},
 };
 
 export default NextAuth(authOptions);
-// adapter: MongoDBAdapter(clientPromise),
-
-// pages: {
-// 	signIn: "/feed",
-// },
-// 		secret: process.env.JWT_SECRET,
-// 		// session: {
-// 		// 	strategy: "jwt",
-// 		// 	maxAge: 30 * 24 * 60 * 60, // 30 Days
-// 		// },
-// 	});
-// }
-// export const authOptions = {
-// 	providers: [
-// 		GoogleProvider({
-// 			clientId: process.env.GOOGLE_CLIENT_ID,
-// 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-// 		}),
-// 	],
-// 	secret: process.env.JWT_SECRET,
-// };
-
-// export default NextAuth(authOptions);
