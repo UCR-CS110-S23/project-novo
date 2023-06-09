a9950c7eae81928bc12d287c9801c883d0d2a204
Viviane Fernando <viviane9533@gmail.com>
2023-06-08 20:15:27 -0700

    Starting from scratch

--- public/script.js
+++
@@ -1,6 +0,0 @@
-// client side javscript
-const socket = io("http://localhost:3030");
-
-socket.on("chat-message", data => {
-	console.log(data);
-});
--- public/server.js
+++
@@ -1,5 +0,0 @@
-const io = require("socket.io")(3030); // pass port that we want server to run on ex.(3000) so creates port on 3000
-
-io.on("connection", socket => {
-	socket.emit("chat-message", "Hello World");
-});
